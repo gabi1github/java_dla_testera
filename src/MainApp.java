01ac4c2b66f15854562fed97734358e49b80b893
gabi1github <gabrielabudzinska95@gmail.com>
2024-08-04 15:08:33 +0200

    moduł 3

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,22 +2,19 @@ public class MainApp {
     
     public static void main(String[] args){
 
-    int a = 10;
-    int b = 20;
-    int c = 11;
-    int d = 12;
+        Bug bug1 = new Bug("testowyopisbledu1", "testowyemail1", 2);
 
-    int sum1 = a+b;
-    boolean bool1 = sum1 % 2 == 0;
-    
-    int sum2 = c+d;
-    boolean bool2 = sum2 % 2 == 0;
-    
-    if (bool1 && bool2) {
-        System.out.println("Obie sumy są parzyste");
-    } else {
-        System.out.println("Nie wszystkie sumy są patrzyste");
+        bug1.getFullInfo();
+        bug1.getEmail();
+        bug1.getStatus();
+        int priority = bug1.getPriority();
+        System.out.println(priority);
+
+        bug1.status = false;
+        bug1.getStatus();
+
     }
-      }
+
+
 
 }
